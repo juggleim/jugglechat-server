9045df91d656337acca1cd8cb0fb3ae187b01e2e
yuwnloy <yuwnloyblog@163.com>
2024-08-06 10:12:47 +0800

    upd

--- services/userservice.go
+++ services/userservice.go
@@ -145,7 +145,7 @@ type User struct {
 type LoginUserResp struct {
 	UserId        string `json:"user_id"`
 	Authorization string `json:"authorization"`
-	NickName      string `json:"nick_name"`
+	NickName      string `json:"nickname"`
 	Avatar        string `json:"avatar"`
 	Status        int    `json:"status"`
 	ImToken       string `json:"im_token,omitempty"`
